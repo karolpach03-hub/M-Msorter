60cf93b8c59958298915e617d6c0e45bc37cb370
karolpach03 <karolpach03@gmail.com>
2026-04-10 11:19:13 +0200

    temporary image display

---
+++ display.py
@@ -0,0 +1,48 @@
+import serial
+import numpy as np
+import cv2
+
+SERIAL_PORT='COM9'
+BAUD_RATE=115200
+WIDTH=128
+HEIGHT=128
+FRAME_SIZE=WIDTH*HEIGHT*2  #2B per px
+
+def process_frame(raw_data):
+    data=np.frombuffer(raw_data, dtype='>u2')
+    r=((data>>11)&0x1F)<<3
+    g=((data>>5)&0x3F)<<2
+    b=((data)&0x1F)<<3
+
+    rgb888=np.stack((b,g,r), axis=-1).astype(np.uint8)
+    rgb888=rgb888.reshape(HEIGHT, WIDTH, 3);
+    rgb888=cv2.resize(rgb888, (4*WIDTH, 4*HEIGHT), interpolation=cv2.INTER_NEAREST)
+    cv2.imshow('ESP cam', rgb888)
+    cv2.waitKey(1)
+
+def main():
+    try:
+        with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=5) as sm:
+            sm.reset_input_buffer()
+            print(f"listening on {SERIAL_PORT}")
+            while True:
+                cv2.waitKey(1)
+                if sm.in_waiting>0:
+                    if sm.read_until(b"FRAME"):
+                        print("Header found")
+                        raw_frame=sm.read(FRAME_SIZE)
+
+                        if len(raw_frame)==FRAME_SIZE:
+                            process_frame(raw_frame)
+                        else:
+                            print(f"Incomplete frame: {len(raw_frame)}/{FRAME_SIZE}")
+        
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(f"Encountered Exception: {e}")
+    finally:
+        cv2.destroyAllWindows()
+
+if __name__=="__main__":
+    main()
--- main.c
+++ main.c
@@ -3,44 +3,108 @@
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
-#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_camera.h"
+#include "driver/uart.h"
 
+#define CAM_PIN_PWDN    -1
+#define CAM_PIN_RESET   -1
+#define CAM_PIN_XCLK    21
+#define CAM_PIN_SIOD    26
+#define CAM_PIN_SIOC    27
+
+#define CAM_PIN_D7      35
+#define CAM_PIN_D6      34
+#define CAM_PIN_D5      39
+#define CAM_PIN_D4      36
+#define CAM_PIN_D3      19
+#define CAM_PIN_D2      18
+#define CAM_PIN_D1       5
+#define CAM_PIN_D0       4
+#define CAM_PIN_VSYNC   25
+#define CAM_PIN_HREF    23
+#define CAM_PIN_PCLK    22
+
+static camera_config_t camera_config={
+    .pin_pwdn=CAM_PIN_PWDN,
+    .pin_reset=CAM_PIN_RESET,
+    .pin_xclk=CAM_PIN_XCLK,
+    .pin_sscb_sda=CAM_PIN_SIOD,
+    .pin_sscb_scl=CAM_PIN_SIOC,
+    .pin_d7=CAM_PIN_D7,
+    .pin_d6=CAM_PIN_D6,
+    .pin_d5=CAM_PIN_D5,
+    .pin_d4=CAM_PIN_D4,
+    .pin_d3=CAM_PIN_D3,
+    .pin_d2=CAM_PIN_D2,
+    .pin_d1=CAM_PIN_D1,
+    .pin_d0=CAM_PIN_D0,
+    .pin_vsync=CAM_PIN_VSYNC,
+    .pin_href=CAM_PIN_HREF,
+    .pin_pclk=CAM_PIN_PCLK,
+    .xclk_freq_hz=20000000,
+    .ledc_timer=LEDC_TIMER_0,
+    .ledc_channel=LEDC_CHANNEL_0,
+    .pixel_format=PIXFORMAT_RGB565,
+    .frame_size=FRAMESIZE_128X128,
+    .fb_count=1,
+    .fb_location=CAMERA_FB_IN_DRAM,
+    .grab_mode=CAMERA_GRAB_WHEN_EMPTY
+};
+
+esp_err_t init_cam()
+{
+    esp_err_t err=esp_camera_init(&camera_config);
+    if(err!=ESP_OK) 
+    {
+        ESP_LOGE("CAM", "Camera init failed with error 0x%x", err);
+        return err;
+    }
+    ESP_LOGI("CAM", "Camera initialized successfully");
+    return ESP_OK;
+}
+
+uart_config_t uart_cfg={
+    .baud_rate=115200,
+    .data_bits=UART_DATA_8_BITS,
+    .parity=UART_PARITY_DISABLE,
+    .stop_bits=UART_STOP_BITS_1,
+    .flow_ctrl=UART_HW_FLOWCTRL_DISABLE,
+    .source_clk=UART_SCLK_DEFAULT
+};
+
+void init_uart()
+{
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_0, 512, 2048, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_cfg));
+}
+
+const char* header="FRAME";
 void app_main(void)
 {
-    printf("Hello world!\n");
+    if(init_cam()!=ESP_OK) return;
+    init_uart();
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+    while(1)
+    {
+        camera_fb_t *fb=esp_camera_fb_get();
+        if(!fb)
+        {
+            ESP_LOGW("CAM","Capture fail");
+        }
+        else
+        {    
+            uart_write_bytes(UART_NUM_0, header, strlen(header));
+            vTaskDelay(pdMS_TO_TICKS(500));
+            uart_write_bytes(UART_NUM_0, (const char*)fb->buf, fb->len);
 
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
+            esp_err_t err=uart_wait_tx_done(UART_NUM_0, pdMS_TO_TICKS(5000));
+            if(err!=ESP_OK)
+            {
+                ESP_LOGW("CAM", "uart_tx wait fail %s", esp_err_to_name(err));
+            }
+            else esp_camera_fb_return(fb);
+        }
+        vTaskDelay(pdMS_TO_TICKS(5000));
     }
-
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
